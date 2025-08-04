ec5bd0cf43e3f57a7f69bc576c594e1e950a5434
TeBeCo <tebeco@gmail.com>
2025-08-05 00:11:38 +0400

    initial support for KnockoutJs

--- scripts/list.js
+++ scripts/list.js
@@ -1,13 +1,12 @@
+function AircraftListViewModel() {
+  this.aircraftList = ko.observableArray([]);
+}
+
+const vm = new AircraftListViewModel();
 fetch('checklists/index.json')
   .then(res => res.json())
   .then(data => {
-    const list = document.getElementById('aircraft-list');
-    data.forEach(entry => {
-      const li = document.createElement('li');
-      const link = document.createElement('a');
-      link.href = `viewer.html?plane=${entry.file}`;
-      link.textContent = entry.name;
-      li.appendChild(link);
-      list.appendChild(li);
-    });
+    vm.aircraftList(data);
   });
+
+ko.applyBindings(vm);
