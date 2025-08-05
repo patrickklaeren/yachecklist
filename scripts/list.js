da2ab914308358ba5043932e941e01f0fc67681c
TeBeCo <tebeco@gmail.com>
2025-08-05 23:01:00 +0400

    add random pr

--- scripts/list.js
+++ scripts/list.js
@@ -1,8 +1,9 @@
-function AircraftListViewModel() {
+function AircraftListViewModel(min, max) {
   this.aircraftList = ko.observableArray([]);
+  this.randomNumber = ko.observable(Math.floor(Math.random() * (max - min + 1)) + min);
 }
 
-const vm = new AircraftListViewModel();
+const vm = new AircraftListViewModel(1, 100); // Change min/max as needed
 fetch('checklists/index.json')
   .then(res => res.json())
   .then(data => {
